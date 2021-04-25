7c9e08710242ca13e9071857cfab4c6881acc033
ravitaagustia <80375049+ravitaagustia@users.noreply.github.com>
2021-04-25 10:58:48 +0700

    Tugas Fragment

---
+++ TugasFragment/app/src/main/java/com/ravita/tugasfragment/HomeFragment.java
@@ -0,0 +1,35 @@
+package com.ravita.tugasfragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+    private TextView textView;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //Komponen
+        textView = view.findViewById(R.id.text);
+        textView.setText("Ravita");
+
+        return view;
+    }
+}
