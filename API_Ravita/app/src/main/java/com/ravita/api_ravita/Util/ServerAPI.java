692218398f49d7f8254869d3048836ae8450d372
ravitaagustia <80375049+ravitaagustia@users.noreply.github.com>
2021-06-09 03:54:37 +0700

    API Volley

---
+++ API_Ravita/app/src/main/java/com/ravita/api_ravita/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.ravita.api_ravita.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.162.161/Minggu11/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.162.161/Minggu11/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.162.161/Minggu11/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.162.161/Minggu11/Mahasiswa/ApiUpdate";
+}
